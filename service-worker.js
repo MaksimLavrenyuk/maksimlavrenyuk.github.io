baa4e44f0c999ae169e46a61e25b5bfccf0b1d5e
MaksimLavrenyuk <42980967+MaksimLavrenyuk@users.noreply.github.com>
2022-06-17 17:15:12 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,1 @@
-importScripts('https://pxl.maksim-lavrenyuk.dev.altkraft.com/service-worker.js?id=MXw5OQ..');
+importScripts('https://pxl.maksim-lavrenyuk.dev.altkraft.com/service-worker.js?id=MXwxMDY.');
